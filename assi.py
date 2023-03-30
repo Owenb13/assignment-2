5bfe54feb1d0cc4b5576dc2347f3d3e4d20dd4d9
Owenb13 <123318047+Owenb13@users.noreply.github.com>
2023-03-30 13:02:35 +0000

    ADDED ASSIGNMENT START

---
+++ assi.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+print("choose one of the following options") # create a list of options for the user to choose from
+print("0. Modulo claculator")
+print("1. integer division calculator")
+print("2. float and integer calculator")
+print("3. for loop counter")
+print("4. integer and float addition")
+print("5. print ascii values of letters in a string")
+print("6. change machine")
+print("7. rock paper scissors")
+print("8. mario stairs")
+user_input= int(input(" What is your selection: "))    # ask the user what operation they would like to choose
+
+if user_input == 0:
+    modulo_calculator()
+
+
+
